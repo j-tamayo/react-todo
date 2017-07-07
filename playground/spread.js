716191b6fda7ba9f3fda9086c6ccabca5324765c
junior.pena <jtamayodev@gmail.com>
2017-07-07 15:15:28 -0400

    Playing with spread operator

---
+++ playground/spread.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(3,1));
+
+var toAdd = [9, 5];
+
+console.log(add(...toAdd));
+
+var groupA = ['jen', 'Cory'];
+var groupB = ['Vikram'];
+var final = [...groupB, 3, ...groupA];
+
+console.log(final);
+
+var person = ['Andrew', 25];
+var personTwo = ['Jen', 29];
+
+function greet(name, age) {
+  console.log('Hi ' + name + ', you are ' + age);
+}
+greet(...person);
+
+var names = ['Mike', 'Ben'];
+var final = ['Junior', ...names];
+final.forEach(function(name) {
+  console.log('Hi ' + name);
+});
